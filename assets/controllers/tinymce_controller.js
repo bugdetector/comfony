492625f26cf846e8310de0439b4026cf0e7cf7ce
Murat Baki Yücel <bakiyucel38@gmail.com>
2024-01-14 20:43:55 +0300

    Tinymce trigger save on change

--- assets/controllers/tinymce_controller.js
+++ assets/controllers/tinymce_controller.js
@@ -6,48 +6,53 @@ import 'tinymce/plugins/image'
 
 // Connects to data-controller="tinymce"
 export default class TinyMceController extends Controller {
-  connect(){
+  connect() {
     var useDarkMode = localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
     const config = {
-        target: this.element,
-        plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons accordion',
-        editimage_cors_hosts: ['picsum.photos'],
-        menubar: 'file edit view insert format tools table help',
-        toolbar: "undo redo | accordion accordionremove | blocks fontfamily fontsize | bold italic underline strikethrough | align numlist bullist | link image table media blockquote | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code visualblocks fullscreen preview | save print | pagebreak anchor codesample | ltr rtl",
-        autosave_ask_before_unload: true,
-        autosave_interval: '30s',
-        autosave_prefix: '{path}{query}-{id}-',
-        autosave_restore_when_empty: false,
-        autosave_retention: '2m',
-        image_advtab: true,
-        importcss_append: true,
-        template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
-        template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
-        height: 600,
-        image_caption: true,
-        quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
-        noneditable_class: 'mceNonEditable',
-        toolbar_mode: 'wrap',
-        contextmenu: 'copy paste link image table',
-        skin: useDarkMode ? 'oxide-dark' : 'oxide',
-        content_css: useDarkMode ? 'dark' : 'default',
-        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
-        branding: false,
-        promotion: false,
+      target: this.element,
+      plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons accordion',
+      editimage_cors_hosts: ['picsum.photos'],
+      menubar: 'file edit view insert format tools table help',
+      toolbar: "undo redo | accordion accordionremove | blocks fontfamily fontsize | bold italic underline strikethrough | align numlist bullist | link image table media blockquote | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code visualblocks fullscreen preview | save print | pagebreak anchor codesample | ltr rtl",
+      autosave_ask_before_unload: true,
+      autosave_interval: '30s',
+      autosave_prefix: '{path}{query}-{id}-',
+      autosave_restore_when_empty: false,
+      autosave_retention: '2m',
+      image_advtab: true,
+      importcss_append: true,
+      template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+      template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+      height: 600,
+      image_caption: true,
+      quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+      noneditable_class: 'mceNonEditable',
+      toolbar_mode: 'wrap',
+      contextmenu: 'copy paste link image table',
+      skin: useDarkMode ? 'oxide-dark' : 'oxide',
+      content_css: useDarkMode ? 'dark' : 'default',
+      content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
+      branding: false,
+      promotion: false,
+      setup: function (editor) {
+        editor.on('change keyup input', function () {
+          editor.save();
+        });
+      }
     };
     tinymce.init(config);
     document.getElementById('theme-toggle').addEventListener('click', () => {
-        setTimeout(() => {
-            useDarkMode = localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-            config.skin = useDarkMode ? 'oxide-dark' : 'oxide';
-            config.content_css = useDarkMode ? 'dark' : 'default';
-            tinymce.remove();
-            tinymce.init(config);
-        })
+      setTimeout(() => {
+        useDarkMode = localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+        config.skin = useDarkMode ? 'oxide-dark' : 'oxide';
+        config.content_css = useDarkMode ? 'dark' : 'default';
+        tinymce.remove();
+        tinymce.init(config);
+      })
     })
   }
 
-  disconnect(){
+  disconnect() {
     tinymce.remove();
   }
 }
