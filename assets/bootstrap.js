3898c7bf5438b79bbf64bcf78471e4e226ffaf3e
Murat Baki Yücel <bakiyucel38@gmail.com>
2024-11-24 02:21:46 +0300

    Form collection type the applied

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,7 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
-import AutosubmitController from './controllers/autosubmit_controller';
 import TinyMceController from './controllers/tinymce_controller';
 import FlashMessageController from './controllers/flash_message_controller';
+import FormCollectionController from './controllers/form_collection_controller';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
@@ -10,6 +10,6 @@ export const app = startStimulusApp(require.context(
     /\.[jt]sx?$/
 ));
 // register any custom, 3rd party controllers here
-app.register('autosubmit', AutosubmitController);
 app.register('tinymce', TinyMceController);
 app.register('flashmessage', FlashMessageController)
+app.register('form-collection', FormCollectionController);
---
+++ assets/controllers/form_collection_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+
+// Connects to data-controller="table-collection"
+export default class FormCollectionController extends Controller {
+  static targets = ["collectionContainer"]
+
+    static values = {
+        index    : Number,
+        prototype: String,
+        containerTag: String,
+    }
+
+    addCollectionItem(event)
+    {
+        const item = document.createElement(this.containerTagValue || 'li');
+        item.innerHTML = this.prototypeValue.replace(/__name__/g, this.indexValue)
+                                            .replace(/__name_index__/g, this.indexValue + 1);
+        item.classList.add('collection-item');
+        this.collectionContainerTarget.appendChild(item);
+        this.indexValue++;
+        initializeComponents()
+    }
+
+    removeCollectionItem(event)
+    {
+      event.target.closest('.collection-item').remove();
+    }
+}
