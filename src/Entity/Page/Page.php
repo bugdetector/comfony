714520ccbf58342e284918dcec83e6e442a5ba4b
Murat Baki Yücel <bakiyucel38@gmail.com>
2025-09-14 23:19:06 +0300

    Page object is translatable

--- src/Entity/Page/Page.php
+++ src/Entity/Page/Page.php
@@ -9,8 +9,9 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Blameable\Traits\BlameableEntity;
-use Gedmo\Mapping\Annotation\Slug;
+use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Gedmo\Translatable\Translatable;
 use Symfony\UX\Turbo\Attribute\Broadcast;
 
 #[ORM\Entity(repositoryClass: PageRepository::class)]
@@ -18,7 +19,8 @@ use Symfony\UX\Turbo\Attribute\Broadcast;
     topics: ['pages'],
     private: true
 )]
-class Page
+#[Gedmo\TranslationEntity(class: PageTranslation::class)]
+class Page implements Translatable
 {
     use TimestampableEntity;
     use BlameableEntity;
@@ -29,13 +31,15 @@ class Page
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Gedmo\Translatable(fallback: true)]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Gedmo\Translatable(fallback: true)]
     private ?string $body = null;
 
     #[ORM\Column(length: 255, unique: true, nullable: true)]
-    #[Slug(fields:['title'])]
+    #[Gedmo\Slug(fields:['title'])]
     private ?string $slug = null;
 
     #[ORM\ManyToMany(targetEntity: File::class)]
@@ -44,9 +48,17 @@ class Page
     #[ORM\Column]
     private ?bool $published = null;
 
+    #[Gedmo\Locale]
+    private ?string $locale = null;
+
+    #[ORM\OneToMany(mappedBy: 'object', targetEntity: PageTranslation::class, cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Collection $translations = null;
+
     public function __construct()
     {
         $this->attachments = new ArrayCollection();
+        $this->translations = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -124,5 +136,15 @@ class Page
         $this->published = $published;
 
         return $this;
+    }
+
+    public function setTranslatableLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    public function getLocale()
+    {
+        return $this->locale;
     }
 }
--- src/Twig/Components/LiveForms/LivePageFormComponent.php
+++ src/Twig/Components/LiveForms/LivePageFormComponent.php
@@ -6,6 +6,7 @@ use App\Entity\Page\Page;
 use App\Form\Page\PageFormType;
 use App\Twig\Components\FormType\LiveAsyncFileInputTrait;
 use Doctrine\ORM\EntityManagerInterface;
+use Gedmo\Translatable\TranslatableListener;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
@@ -25,16 +26,27 @@ final class LivePageFormComponent extends AbstractController
     #[LiveProp()]
     public ?Page $initialFormData = null;
 
+    #[LiveProp(url: true, writable: true, onUpdated: 'onLocaleUpdated')]
+    public ?string $locale = null;
+
+    public string $defaultLocale;
+
     public function __construct(
         protected EntityManagerInterface $entityManager,
         protected TranslatorInterface $translator,
+        string $locale,
     ) {
+        $this->defaultLocale = $locale;
+        $this->locale = $locale;
     }
 
     protected function instantiateForm(): FormInterface
     {
         if (!$this->initialFormData) {
             $this->initialFormData = new Page();
+        } elseif ($this->locale && $this->initialFormData->getId()) {
+            $this->getTranslatableListener()->setTranslatableLocale($this->locale);
+            $this->entityManager->refresh($this->initialFormData);
         }
         $form = $this->createForm(PageFormType::class, $this->initialFormData, [
             'attr' => [
@@ -52,6 +64,7 @@ final class LivePageFormComponent extends AbstractController
         $this->submitForm();
         /** @var Page */
         $object = $this->getForm()->getData();
+        $object->setTranslatableLocale($this->locale);
         $isCreate = !boolval($object->getId());
         $this->entityManager->persist($object);
         $this->entityManager->flush();
@@ -62,5 +75,22 @@ final class LivePageFormComponent extends AbstractController
         if ($isCreate) {
             return $this->redirectToRoute('app_admin_page_edit', ['id' => $object->getId()]);
         }
+    }
+
+    private function getTranslatableListener(): ?TranslatableListener
+    {
+        foreach ($this->entityManager->getEventManager()->getAllListeners() as $listeners) {
+            foreach ($listeners as $listener) {
+                if ($listener instanceof TranslatableListener) {
+                    return $listener;
+                }
+            }
+        }
+        return null;
+    }
+
+    public function onLocaleUpdated()
+    {
+        $this->resetForm();
     }
 }
